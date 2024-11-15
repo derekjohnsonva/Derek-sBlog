4564dfba1bc69aa12dca98a40c2789d0ae9f4456
Derek Johnson <dej3tc@virginia.edu>
2024-11-15 15:36:35 -0500

    change metadata

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,6 +1,6 @@
 export default {
 	title: "Derek's Blog",
-	url: "",
+	url: "Derek-sBlog",
 	language: "en",
 	description: "This is in no way a focused effort. I may not ever post. But I think it is good to have a little landing page for my thoughts",
 	author: {
